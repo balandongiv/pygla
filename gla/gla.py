208c5a6d53cd190984da227ef5b5a8ee4b6cb3a5
balandongiv <balandongiv@gmail.com>
2023-02-02 13:04:17 +0800

    Update documentation

--- gla/gla.py
+++ gla/gla.py
@@ -208,16 +208,6 @@ def create_parser():
     return parser
 
 
-def tst_fill_excel():
-    fexcel = r'C:\Users\balandongiv\IdeaProjects\pygla\unit_test\peer_transform _22_23_fill.xlsx'
-
-    peer_evaluator = PeerEvaluator(pd.read_excel(fexcel))
-    peer_evaluator.check_missing_infomation()
-    peer_evaluator.re_arrange_justification()
-    peer_evaluator.convert_percentage_weightage(scale_type=5)
-
-    df = peer_evaluator.aggregate_each_peers()
-    save_output_excel(df, fname='2aaa023_11.xlsx', verbose=False)
 
 
 if __name__ == '__main__':
